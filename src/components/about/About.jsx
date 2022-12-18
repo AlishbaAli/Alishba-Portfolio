bbc70be151ae3e603b90fb1f6439a5c8cf7cfced
Alishba <alishba5ahmedali@gmail.com>
2022-12-18 14:36:36 -0500

    first deploy

---
+++ src/components/about/About.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/about-me2.png'
+import {FaAward} from 'react-icons/fa'
+import {FaFolder} from 'react-icons/fa'
+
+const About = () => {
+  return (
+    <section id='about'>
+    <h5>Get to Know</h5>
+    <h2>About Me</h2>
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt="About Image" />
+        </div>
+      </div>
+      <div className="about__content">
+        <div className="about__cards">
+          <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>3+ years Working</small>
+          </article>
+          <article className='about__card'>
+            <FaFolder className='about__icon'/>
+            <h5>Projects</h5>
+            <small>10+ Completed</small>
+          </article>
+        </div>
+        {/* <p>
+          project details
+          project details
+          project details
+          project details
+          project details<br>
+          </br>
+          project details
+          project details
+          project details
+          project details
+        </p> */}
+        <p>
+          
+        </p>
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      </div>
+    </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './footer.css'
+import {BsFacebook} from 'react-icons/bs'
+import {AiOutlineInstagram} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+   <footer>
+    {/* <a href="#" className='footer__logo'>Alishba</a> */}
+    <ul className='permalinks'>
+      <li><a href="#">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+
+    </ul>
+    <div className="footer__socials">
+      <a href="#"> <BsFacebook /></a>
+      <a href="#"> <AiOutlineInstagram /></a>
+    </div>
+    <div className="footer__copyright">
+      <small>&copy; Alishba Portfolio. All rights reserved.</small>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
